c2ce5b1bfe4dda0a31dcda1e34b0b3e61188c765
Easwari Ramani <eshuprad@gmail.com>
2018-01-05 13:35:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function BoardMember(names, homeState,training){
     return "I like to help people.";
   };
   this.releasePressStatement=function(){
-    return "You will see great things from Scober";
+    return "You will see great things from Scober.";
   };
   this.sayHi=funciton(){
     return `Hi, my name is ${this.names}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
