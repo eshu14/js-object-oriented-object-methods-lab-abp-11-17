befb290c81266d0b66b48985833f2428d5ab3e0b
Easwari Ramani <eshuprad@gmail.com>
2018-01-05 13:32:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ function BoardMember(names, homeState,training){
     return 'No, I must disagree';
   };
   this.approve=function(){
-    return 'You can do that!';
+    return "You can do that!";
   };
   this.doCharity=function(){
-    return 'I like to help people';
+    return "I like to help people";
   };
   this.releasePressStatement=function(){
-    return 'You will see great things from Scober';
+    return "You will see great things from Scober";
   };
   this.sayHi=funciton(){
     return `Hi, my name is ${this.names}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
